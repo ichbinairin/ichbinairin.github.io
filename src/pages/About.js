9aa15eab00839705deb9968ea56a8e0b291ef060
Irina Georgescu <ge.irina25@gmail.com>
2023-06-13 23:07:08 +0300

    ui updates

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+function About() {
+  return (
+    <div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar, odio et posuere elementum, erat enim blandit ante, quis hendrerit lorem eros sed quam. Phasellus mollis et dolor a dapibus. Mauris aliquet lacus neque, scelerisque ultrices dolor vestibulum eget. Aliquam suscipit maximus semper. Sed porta gravida diam, quis imperdiet quam gravida non. Etiam ligula orci, cursus id faucibus at, scelerisque sit amet elit. Nullam bibendum sodales diam vitae cursus.
+
+        Donec a cursus enim. Sed ante risus, finibus quis finibus id, ultrices sit amet risus. Ut sit amet turpis posuere, commodo felis non, egestas leo. Sed a vehicula diam, vitae sollicitudin nunc. Aenean finibus vulputate luctus. Curabitur pretium tellus sit amet dictum mattis. Integer at porttitor est, eget ultricies tellus. Aliquam commodo tempus orci, et placerat nisi posuere gravida. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut feugiat turpis sed nunc commodo ullamcorper. Proin elit nibh, dictum sed volutpat id, imperdiet eget leo. Integer elementum purus odio, ornare condimentum eros tempor at. In fringilla, elit molestie fermentum placerat, lacus nisl interdum purus, in pellentesque nibh nisi sit amet purus. Fusce finibus semper viverra. Suspendisse lacinia purus at risus porttitor dictum ut sit amet dui.
+
+        Aenean dictum nulla id urna varius, quis molestie mauris suscipit. Sed elit metus, rutrum sed neque nec, porta auctor orci. Nam et imperdiet neque. Aliquam vitae erat ex. Ut mi diam, venenatis id sodales in, dignissim eget augue. Phasellus lobortis, sem ut tempor venenatis, risus libero interdum odio, in vulputate turpis ligula eget metus. Sed elementum rutrum tincidunt. Duis molestie mollis tristique. Morbi aliquam lacinia rhoncus. Aliquam erat volutpat.
+
+        Vestibulum imperdiet tincidunt nunc, eget sodales justo ullamcorper vitae. Integer mattis arcu eget suscipit ornare. Duis nec porttitor risus. Morbi ultrices convallis nibh in varius. Duis bibendum sagittis maximus. Integer interdum risus ex, eu convallis velit euismod sit amet. Cras convallis, risus nec volutpat interdum, justo magna iaculis quam, vel tristique libero tellus id est. Nunc in tempus eros, eu fringilla purus. Nullam vel aliquet dolor. Nam sed luctus turpis, a ornare justo. In non tristique sem, ac ullamcorper urna. Suspendisse pulvinar turpis augue, sed convallis neque tempor nec.
+      </div>
+    </div>
+  )
+}
+export default About
